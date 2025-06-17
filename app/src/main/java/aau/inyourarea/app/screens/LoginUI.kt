f07f25fb7f142101c5122ac4ab007124294de4ee
davensive <essl.david@gmail.com>
2025-06-17 20:45:14 +0200

    LoginUIscreen to Kotlin, Textfields and LoginButton added

--- app/src/main/java/aau/inyourarea/app/screens/LoginUI.kt
+++ app/src/main/java/aau/inyourarea/app/screens/LoginUI.kt
@@ -1,4 +1,105 @@
-package aau.inyourarea.app.screens;
+package aau.inyourarea.app.screens
 
-public class LoginUI {
+import aau.inyourarea.app.network.NetworkService
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.util.concurrent.CompletableFuture
+
+class LoginUI : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            LoginScreen()
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var loginStatus by remember { mutableStateOf<String?>(null) }
+
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(modifier=Modifier.fillMaxWidth(),Arrangement.SpaceEvenly) {
+                Button(
+                    onClick = {
+                        // .getInstance().service
+                        NetworkService()
+
+                            .login(username, password, false)
+                            .thenAccept { success ->
+                                if (success) {
+                                    loginStatus = "Login erfolgreich"
+                                } else {
+                                    loginStatus = "Login fehlgeschlagen"
+                                }
+                            }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.DarkGray,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text("Login")
+                }
+
+
+
+                loginStatus?.let {
+
+                    Text(text = it)
+                }
+           // Button() { }
+            // Register button
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
