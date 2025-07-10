3638bed2b5b9ef48b3e8c1758e2a87da0a6de5e1
davensive <essl.david@gmail.com>
2025-07-10 17:48:29 +0200

    background for LoginScreen and further design

--- app/src/main/java/aau/inyourarea/app/screens/LoginUI.kt
+++ app/src/main/java/aau/inyourarea/app/screens/LoginUI.kt
@@ -1,5 +1,6 @@
 package aau.inyourarea.app.screens
 import aau.inyourarea.app.network.NetworkService
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -12,8 +13,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
 
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -28,11 +36,29 @@ fun LoginScreen(navController: NavController) {
     var password by remember { mutableStateOf("") }
     var loginStatus by remember { mutableStateOf<String?>(null) }
     var showLoginScreen by remember { mutableStateOf(false) }
-    Scaffold { paddingValues ->
+  Box(modifier=Modifier.fillMaxSize()){
+      Image(painter=painterResource(id = aau.inyourarea.app.R.drawable.key),
+          contentDescription = "Background Image",
+          modifier = Modifier.fillMaxSize(),
+          contentScale = ContentScale.Crop)
+  }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Black.copy(alpha = 0.50f), // oben dunkel
+                        Color.White.copy(alpha = 0.500f), // Mitte dunkler als vorher
+                        Color.White.copy(alpha = 0.500f),
+                        Color.Black.copy(alpha = 0.100f)  // unten dunkel
+                    )
+                )
+            )
+    )
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -49,6 +75,7 @@ fun LoginScreen(navController: NavController) {
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") }
+
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -72,7 +99,7 @@ fun LoginScreen(navController: NavController) {
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.DarkGray,
                         contentColor = Color.White
-                    ),modifier = Modifier.fillMaxWidth(),
+                    ),
                     enabled = username.isNotBlank() && password.isNotBlank()
                 ) {
                     Text("Login")
@@ -84,6 +111,7 @@ fun LoginScreen(navController: NavController) {
 
                     Text(text = it)
                 }
+
                 Button(
                     onClick = {
                         // .getInstance().service
@@ -105,17 +133,20 @@ fun LoginScreen(navController: NavController) {
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.DarkGray,
                         contentColor = Color.White
-                    ),modifier = Modifier.fillMaxWidth(),
+                    ),
                     enabled = username.isNotBlank() && password.isNotBlank()
                 ) {
                     Text("Registrieren")
+                }
+
+
                 }
             }
         }
 
 
-            }
-        }
+            
+
 @Preview(showBackground = true)
 @Composable
 fun LoginScreenPreview() {
