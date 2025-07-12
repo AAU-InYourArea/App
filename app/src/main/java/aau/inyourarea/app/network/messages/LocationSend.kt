fbcb9eabe1490aa9bcedc3b84b0ec53e87686484
davensive <essl.david@gmail.com>
2025-07-12 18:10:00 +0200

    getInstance from Networkservice and sendcommand added

--- app/src/main/java/aau/inyourarea/app/network/messages/LocationSend.kt
+++ app/src/main/java/aau/inyourarea/app/network/messages/LocationSend.kt
@@ -1,24 +1,39 @@
 package aau.inyourarea.app.network.messages
 
 
+import aau.inyourarea.app.network.CommandType
 import android.content.Context
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import android.location.Location
-import androidx.compose.runtime.mutableStateOf
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
 import com.google.android.gms.location.LocationServices
 import aau.inyourarea.app.network.NetworkService
+import aau.inyourarea.app.network.getNetworkService
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.util.Log
+import androidx.activity.ComponentActivity
 
-class LocationSend(private val context: Context) {
-    private val networkService = NetworkService() // get NetworkService instance Fix!!!
+class LocationSend(private val context: Context):ComponentActivity() {
+    override fun onStart() {
+        super.onStart()
+        Intent(this, NetworkService::class.java).also {
+            startService(it)
+            bindService(it, networkServiceHolder.connection, BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(networkServiceHolder.connection)
+    }
     private val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
-    
+    val networkServiceHolder = getNetworkService()
+
     private val locationRequest = LocationRequest.Builder(
         Priority.PRIORITY_HIGH_ACCURACY,
         10000L
@@ -31,8 +46,8 @@ class LocationSend(private val context: Context) {
             val longitude = location.longitude
 
             val payload = LocationPayLoad(latitude, longitude)
+            networkServiceHolder.service.sendCommand(CommandType.UPDATE_LOCATION,payload)
 
-            // Hier wird die Payload an den NetworkService gesendet
         }
     }
     @SuppressLint("MissingPermission")
@@ -45,6 +60,7 @@ class LocationSend(private val context: Context) {
         fusedLocationClient.removeLocationUpdates(locationCallback)
         Log.d("LocationSend", "Stopped location updates")
     }
+
 }
 data class LocationPayLoad(
     val latitude: Double,
